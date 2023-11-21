5e407b1023663b6e5bd9a8b80936c43d1798d125
Bimal Rajapakse <berajapakse@gmail.com>
2023-11-21 16:22:46 +0530

    finished skip ui

---
+++ resources/views/jd/header/skipheader.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary" style="margin-bottom:0px; background:transparent !important;">
+    <div class="container-fluid mynav">
+    <div class="contactBox">
+    
+    </div>
+    <!--<div class="logoText">Job Daddy</div>-->
+    <a class="navbar-brand" href="/">
+        <img class="companyLogo" src="/jd_img/monkey.png" alt="">
+        <div class="logoText">Job Daddy</div>
+    </a>
+
+
+    <div class="loginAndLangLinks">
+        <div>
+            <form action="#">
+                <select name="lang" id="lang">
+                  <option value="volvo">En</option>
+                  <option value="saab">Sin</option>
+                  <option value="opel">Tm</option>
+                </select>
+              </form>
+        </div>
+        
+    <ul class="navbar-nav ms-auto">
+      
+    </ul>
+    </div>
+
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    </div>  
+</nav>
